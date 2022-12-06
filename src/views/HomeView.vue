f2a50569c067e71bce9be223408e6105f702af0e
Burunoheck <brunoreeck6@gmail.com>
2022-12-05 21:18:38 -0300

    tentativa falha

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,33 @@
   <v-container class="pa-12" fluid>
     <h1 class="h1">Lista com tarefas adicionáveis</h1>
     <v-form>
-      <v-text-field @click:append-inner="adicionar" variant="solo" append-inner-icon="mdi-send" label="Nova Tarefa" v-model="novaTarefa" @keydown.enter="adicionar"></v-text-field>
+      <v-text-field
+        @click:append-inner="adicionar"
+        variant="solo"
+        append-inner-icon="mdi-send"
+        label="Nova Tarefa"
+        v-model="novaTarefa"
+        @keydown.enter="adicionar"
+      ></v-text-field>
     </v-form>
-      <v-divider>
-      </v-divider>
-      <v-list>
-        <v-list-item-group>
-          <v-list-item class="ma-1" variant="outlined" v-for="tarefa of tarefas" :key="tarefa.titulo">
-            <v-card>
+    <v-divider> </v-divider>
+    <v-list>
+      <v-list-item-group>
+        <v-list-item
+          class="ma-1"
+          variant="outlined"
+          v-for="(tarefa, i) of tarefas"
+          :key="i"
+        >
+          <v-card class="pl-3">
             {{ tarefa.titulo }}
+            <v-btn @click="removeTarefas(tarefa)">
+              <v-icon>mdi-trash-can</v-icon>
+            </v-btn>
           </v-card>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
   </v-container>
 </template>
 
@@ -22,24 +36,28 @@
 export default {
   data() {
     return {
-      novaTarefa: "",  
+      novaTarefa: "",
       tarefas: [
-        {titulo: "tarefa 1"},
-        {titulo: "tarefa 2"},
-        {titulo: "tarefa 3"},
-        ],
+        { titulo: "matemática", id: 1},
+        { titulo: "física", id: 2},
+        { titulo: "biologia", id: 3},
+      ],
     };
   },
   methods: {
     adicionar(e) {
       e.preventDefault();
-      this.tarefas.push({titulo: this.novaTarefa})
+      this.tarefas.push({ titulo: this.novaTarefa, id: this.tarefas.length + 1});
       this.novaTarefa = "";
-    }
-  }
+    },
+    removeTarefas({ id }) {
+      const index = this.tarefas.findIndex((item) => item.id === id);
+      if (index > -1) {
+        delete this.tarefas[index]
+      }
+    },
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
